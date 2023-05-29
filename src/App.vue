1cea40cd6601fb2a880dbd17377a7c57094c06c6
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-05-29 15:09:16 +0200

    feat: installed axios, created ProjectCard component, axios api call to get all the projects

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,42 @@
 <script>
 
+import axios from 'axios';
+
+import ProjectCard from './components/ProjectCard.vue';
+
 export default {
   name: 'App',
 
   data() {
-    return
+    return {
 
-  }
+      projects: [],
+
+    }
+  },
+
+  components: {
+    ProjectCard,
+  },
+
+  created() {
+
+    this.getProjects();
+
+  },
+
+  methods: {
+
+    getProjects() {
+
+      axios.get('http://127.0.0.1:8000/api/projects').then(response => {
+        // console.log(response.data.results);
+        this.projects = response.data.results;
+      });
+
+    }
+
+  },
 }
 
 </script>
@@ -14,7 +44,15 @@ export default {
 <template>
   <div class="container pt-5">
     <h1>Tutti i progetti</h1>
+
+    <hr>
+
+    <div class="row">
+      <div v-for="project in projects" class="col-4 mb-3">
+        <ProjectCard :project="project"></ProjectCard>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+  name: 'ProjectCard',
+
+  data() {
+    return {
+
+    }
+  },
+
+  props: {
+    project: Object,
+  },
+
+  computed: {
+
+    // tronco la descrizione se è più lunga di 50 caratteri
+    shortContent() {
+      if (this.project.description.length > 50) {
+        return this.project.description.substring(0, 50) + '...';
+      } else {
+        return this.project.description;
+      }
+    },
+
+
+  },
+}
+
+</script>
+
+<template>
+  <div class="card">
+    <!-- <img src="..." class="card-img-top" alt="..."> -->
+    <div class="card-body">
+      <h5 class="card-title">{{ project.title }}</h5>
+      <p class="card-text">{{ shortContent }}</p>
+      <!-- <a href="#" class="btn btn-primary">Go</a> -->
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
