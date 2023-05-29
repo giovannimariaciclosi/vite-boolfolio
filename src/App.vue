6e834ef0945d72ca520d3e8ed72b2e98a7d19b65
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-05-29 14:32:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+  name: 'App',
+
+  data() {
+    return
+
+  }
+}
+
+</script>
+
+<template>
+  <div class="container pt-5">
+    <h1>Tutti i progetti</h1>
+  </div>
+</template>
+
+<style scoped></style>
