5ef23a03320036a343b21236202a2be3b5ff2456
giovannimariaciclosi <giovannimariaciclosi@gmail.com>
2023-05-31 14:21:47 +0200

    feat: added dynamic meta

--- src/router.js
+++ src/router.js
@@ -12,18 +12,31 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: AppHome,
+      meta: {
+        title: "Giovanni Maria Ciclosi | Portfolio",
+      },
     },
     {
       path: "/about-us",
       name: "about",
       component: AppAbout,
+      meta: {
+        title: "Chi sono | Portfolio",
+      },
     },
     {
       path: "/projects",
       name: "projects.index",
       component: ProjectsIndex,
+      meta: {
+        title: "I miei progetti | Portfolio",
+      },
     },
   ],
+});
+
+router.beforeEach((to, from) => {
+  document.title = to.meta?.title ?? "Giovanni Maria Ciclosi | Portfolio";
 });
 
 export { router };
